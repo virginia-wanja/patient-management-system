f9ac211e8af7d38139a034293988ae679cade329
Virginia <kagundavirginia4@gmail.com>
2025-09-29 17:34:44 +0400

    Fix form field mismatch causing Bad Request

--- app.py
+++ app.py
@@ -4,9 +4,15 @@ app = Flask(__name__)
 patients = []
 @app.route("/", methods=["GET", "POST"])
 def patients_view():
-    selected_condition = None
-    filtered_patients = patients
     if request.method == "POST":
-        selected_condition = request.form["condition"]
-        filtered_patients = [patient for patient in patients if patient["condition"] == selected_condition]
-    return render_template("patient.html", patients=filtered_patients)
+        name = request.form["name"]
+        age = request.form["age"]
+        gender = request.form["gender"]
+        address = request.form["address"]
+        patients.append({
+            "name": name,
+            "age": age,
+            "gender": gender,
+            "address": address
+        })
+    return render_template("patient.html", patients=patients)
